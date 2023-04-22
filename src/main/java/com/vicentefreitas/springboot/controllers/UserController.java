d04e4855e8265b6711ad318955df1ac75ddbee8f
Vicente Freitas <vicenteteixeirafreitas@gmail.com>
2023-04-22 00:16:48 -0300

    Removing data from an entity

--- src/main/java/com/vicentefreitas/springboot/controllers/UserController.java
+++ src/main/java/com/vicentefreitas/springboot/controllers/UserController.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
@@ -43,5 +44,14 @@ public class UserController {
 		model.addAttribute("users", userRepository.findAll());
 		return "/auth/admin/admin-list-user";
 	}
+	
+	@GetMapping("/admin/apagar/{id}")
+	public String deleteUser(@PathVariable("id") long id, Model model) {
+		User user = userRepository.findById(id)
+				.orElseThrow(() -> new IllegalArgumentException("Id inválido:" + id));
+		userRepository.delete(user);
+	    return "redirect:/usuarios/admin/listar";
+	}
+
 }
 	
