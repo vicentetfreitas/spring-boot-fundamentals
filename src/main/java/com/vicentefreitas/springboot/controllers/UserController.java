3f1216c9fee2143f899f56e6a652c0fd80572ac9
Vicente Freitas <vicenteteixeirafreitas@gmail.com>
2023-04-23 12:12:53 -0300

    Changing user registration, checking login and assigning user role to new records

--- src/main/java/com/vicentefreitas/springboot/controllers/UserController.java
+++ src/main/java/com/vicentefreitas/springboot/controllers/UserController.java
@@ -1,5 +1,7 @@
 package com.vicentefreitas.springboot.controllers;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
 import javax.validation.Valid;
@@ -14,71 +16,101 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import com.vicentefreitas.springboot.models.Paper;
 import com.vicentefreitas.springboot.models.User;
+import com.vicentefreitas.springboot.repositories.PaperRepository;
 import com.vicentefreitas.springboot.repositories.UserRepository;
 
 @Controller
 @RequestMapping("/usuarios")
 public class UserController {
-	
+
 	@Autowired
 	private UserRepository userRepository;
+
+	@Autowired
+	private PaperRepository paperRepository;
 
 	@GetMapping("/novo")
 	public String addUser(Model model) {
 		model.addAttribute("user", new User());
 		return "/publish-create-user";
 	}
-	
+
 	@PostMapping("/salvar")
-	public String saveUser(@Valid User user, BindingResult result, RedirectAttributes attributes) {
-		 if(result.hasErrors()) {
-			 return "publish-create-user";
-		 }
-		 userRepository.save(user);
-		 attributes.addFlashAttribute("message", "Usuário salvo com sucesso!!");
-		 return "redirect:/usuarios/novo";
-		
+	public String saveUser(@Valid User user, BindingResult result, Model model, RedirectAttributes attributes) {
+
+		if (result.hasErrors()) {
+
+			return "publish-create-user";
+		}
+
+		User usr = userRepository.findByLogin(user.getLogin());
+
+		if (usr != null) {
+			model.addAttribute("loginExist", "Login já cadastrado");
+
+			return "publish-create-user";
+		}
+
+		Paper paper = paperRepository.findByPaper("USER");
+		List<Paper> papers = new ArrayList<Paper>();
+		papers.add(paper);
+		user.setPapers(papers);
+		userRepository.save(user);
+		attributes.addFlashAttribute("message", "Usuário salvo com sucesso!!");
+
+		return "redirect:/usuarios/novo";
+
 	}
-	
+
 	@RequestMapping("/admin/listar")
 	public String listUser(Model model) {
+
 		model.addAttribute("users", userRepository.findAll());
+
 		return "/auth/admin/admin-list-user";
+
 	}
-	
+
 	@GetMapping("/admin/apagar/{id}")
 	public String deleteUser(@PathVariable("id") long id, Model model) {
-		User user = userRepository.findById(id)
-				.orElseThrow(() -> new IllegalArgumentException("Id inválido:" + id));
+
+		User user = userRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("Id inválido:" + id));
 		userRepository.delete(user);
-	    return "redirect:/usuarios/admin/listar";
+
+		return "redirect:/usuarios/admin/listar";
+
 	}
-	
+
 	@GetMapping("/editar/{id}")
 	public String editUser(@PathVariable("id") long id, Model model) {
+
 		Optional<User> userOld = userRepository.findById(id);
+
 		if (!userOld.isPresent()) {
-            throw new IllegalArgumentException("Usuário inválido:" + id);
-        } 
+			throw new IllegalArgumentException("Usuário inválido:" + id);
+		}
+
 		User user = userOld.get();
-	    model.addAttribute("user", user);
-	    return "/auth/user/user-alter-user";
+		model.addAttribute("user", user);
+
+		return "/auth/user/user-alter-user";
 
 	}
-	
+
 	@PostMapping("/editar/{id}")
-	public String editUser(@PathVariable("id") long id, 
-			@Valid User user, BindingResult result) {
+	public String editUser(@PathVariable("id") long id, @Valid User user, BindingResult result) {
+
 		if (result.hasErrors()) {
 			user.setId(id);
-	        return "/auth/user/admin-list-user";
-	    }
-	    userRepository.save(user);
-	    return "redirect:/usuarios/admin/listar";
+
+			return "/auth/user/admin-list-user";
+		}
+
+		userRepository.save(user);
+
+		return "redirect:/usuarios/admin/listar";
 	}
 
-	
-
 }
-	
--- src/main/java/com/vicentefreitas/springboot/repositories/UserRepository.java
+++ src/main/java/com/vicentefreitas/springboot/repositories/UserRepository.java
@@ -5,5 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.vicentefreitas.springboot.models.User;
 
 public interface UserRepository extends JpaRepository<User, Long>{
+	
+	User findByLogin(String login);
 
 }
